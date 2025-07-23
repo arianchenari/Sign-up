11a3bbfedb5ded3f1ccc9497cadbc224ebb822d0
Arian Chenari <chenariarian2@gmail.com>
2025-07-23 05:31:28 -0700

    MVC-JS for feature section

---
+++ scripts/cta-cart.js
@@ -0,0 +1,61 @@
+import { usersInfo } from "./data/users-informations.js";
+
+renderCarts();
+
+function renderCarts() {
+    let cartsHTML = '';
+    let buttonsHTML = '';
+
+    usersInfo.forEach((user, index) => {
+        cartsHTML +=`
+            <div class="CTA-cart js-CTA-cart-${user.id} js-display-cart">
+                <div class="user-information">
+                    <img src="${user.image}" class="person-image">
+                    <div class="username-job">
+                        <h2>${user.name}</h2>
+                        <span>${user.job}</span>
+                    </div>
+                </div>
+                <p>${user.comment}</p>
+            </div>
+        `;
+        if(index <= 3){
+            buttonsHTML +=`
+            <button class="selection js-selection js-selection-${user.id}" data-user-id="${user.id}"></button>
+            `;
+        };
+    });
+
+    document.querySelector('.js-cart-list')
+      .innerHTML = cartsHTML;
+
+    document.querySelector('.js-selectors-container')
+      .innerHTML = buttonsHTML;
+
+    showCart(usersInfo[0].id);
+    function showCart(userId) {
+        removesShowCarts();
+        document.querySelector(`.js-selection-${userId}`).classList.add('checked');
+        document.querySelector(`.js-CTA-cart-${userId}`).classList.add('js-display-cart');
+    };
+
+    function removesShowCarts() {
+        document.querySelectorAll('.js-selection').forEach((button) => {
+            const userId = button.dataset.userId;
+            usersInfo.forEach((user) => {
+                if(user.id === userId){
+                document.querySelector(`.js-selection-${userId}`).classList.remove('checked');
+                document.querySelector(`.js-CTA-cart-${userId}`).classList.remove('js-display-cart');
+                };
+            })
+        });
+    };
+
+    document.querySelectorAll('.js-selection').forEach((button) => {
+        button.addEventListener('click', () => {
+            const userId = button.dataset.userId;
+            showCart(userId);
+        });
+    });
+}
+
---
+++ scripts/data/users-informations.js
@@ -0,0 +1,25 @@
+export const usersInfo = [{
+    name: 'Sara Labora',
+    job: 'Student & CEO',
+    image: 'images/sara-personal-image.jpg',
+    comment: 'i love this site and enjoy that it`s helpful! i love this site and enjoy that it`s helpful! i love this site and enjoy that it`s helpful!',
+    id: '1'
+}, {
+    name: 'Adam Moradi',
+    job: 'Trader & Saler',
+    image: 'images/adam-personal-image.jpg',
+    comment: 'i really love this site it`s help me to solve my problem and i suggest this site to everyone to want love this footbal!!',
+    id: '2'
+}, {
+    name: 'Shahin Azarseresht',
+    job: 'Koskesh do allam & Bahare Lover!',
+    image: 'images/shahin-personal-image.jpg',
+    comment: 'so how many kilos are u ? stciker<:pishi:> best world advisor',
+    id: '3'
+}, {
+   name: 'Arian Chenari',
+    job: 'Friend of koskeshe',
+    image: 'images/arian-personal-image.jpg',
+    comment: 'i don`t know why shahin is my friend :/ so what you think about him ?! can u shower cold wather?',
+    id: '4'
+}];
